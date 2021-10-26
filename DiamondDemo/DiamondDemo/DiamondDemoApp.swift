3f13e75e5a2c0c9299523c2e53e325ba0a2e0863
LeeAnn <leewarren2016@gmail.com>
2021-10-26 12:10:55 -0500

    imported init project files
    DiamondDemo import for git source control

---
+++ DiamondDemo/DiamondDemo/DiamondDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiamondDemoApp.swift
+//  DiamondDemo
+//
+//  Created by Leeann Warren on 10/26/21.
+//
+
+import SwiftUI
+
+@main
+struct DiamondDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
