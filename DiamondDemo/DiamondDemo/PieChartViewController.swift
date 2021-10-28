da687cf6e186116bb8b85aca63d28213bab2fee8
coralLemons <warrenlc1@jacks.sfasu.edu>
2021-10-28 09:48:40 -0500

    pie file updated

--- DiamondDemo/DiamondDemo/PieChartViewController.swift
+++ DiamondDemo/DiamondDemo/PieChartViewController.swift
@@ -18,16 +18,25 @@ class PieChartViewController: UIViewController{
         customizeChart(dataPoints: users, values: screenTimeHours)
     }// end ViewDidLoad
     
+    
     func customizeChart(dataPoints: [String], values: [Double]){
         
         var entries: [ChartDataEntry] = [] // holds the data entries for the chart
         for value in 0..<dataPoints.count {
             let dataEntry = PieChartDataEntry(value: values[value], label: dataPoints[value], data: dataPoints[value] as AnyObject)
             entries.append(dataEntry) // add dataEntry to the end of the ChartData array
-        }// end for loop --
+        }// end for loop 
         
         let pieChartDataSet = PieChartDataSet(entries: entries, label: nil)
         pieChartDataSet.colors = getColor(numbersOfColor: dataPoints.count)
+        
+        let pieChartData = PieChartData(dataSet: pieChartDataSet)
+        let format = NumberFormatter()
+        format.numberStyle = .none
+        let formatter = DefaultValueFormatter(formatter: format)
+        pieChartData.setValueFormatter(formatter)
+            
+        pieChartView.data = pieChartData
         
     }// end customizeChart -- pass in dataPoints and applicable values to populate (customize) the pie chart
     
